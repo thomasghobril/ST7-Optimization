f48288d1bc04a5ed1eeb0b427805d7ccab82f950
nyss <ines.dormoy@student-cs.fr>
2022-02-02 16:55:50 +0100

    avant merge

--- read_input.py
+++ read_input.py
@@ -29,18 +29,17 @@ def read_unavalabilities():
     wookbook = openpyxl.load_workbook("InstancesV1/InstanceBordeauxV1.xlsx")
 
     # Define variable to read the active sheet:
-    worksheet = wookbook.active
+    worksheet = wookbook['Employees Unavailabilities']
 
     # Iterate the loop to read the cell values
-
-
     for row in worksheet.iter_rows(2, max_row=15):
         if row[0].value == None:
             break
         EmployeeName = row[0].value
         latitude = row[1].value
         longitude = row[2].value
-        skill = row[3].value
-        level = row[4].value
-        workingStartTime = row[4].value
-        workingEndTime = row[6].value
+        start = row[3].value
+        end = row[4].value
+
+    print('ok')
+read_unavalabilities()
