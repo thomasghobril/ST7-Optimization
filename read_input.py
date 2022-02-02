1c346dad8681debb1c3485dd2ce4a385d1c34c60
nyss <ines.dormoy@student-cs.fr>
2022-02-02 17:07:50 +0100

    read_input finish

--- read_input.py
+++ read_input.py
@@ -1,3 +1,4 @@
+# Import openyxl module
 import openpyxl
 from classes import Ressource,RessourceUnavailibility,Task,TaskUnavailibility
 
@@ -6,6 +7,7 @@ def read_ressources(v):
     ressources={}
     # Define variable to load the wookbook
     wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/InstanceBordeauxV"+str(v)+".xlsx")
+
     # Define variable to read the active sheet:
     worksheet = wookbook.active
 
@@ -16,10 +18,10 @@ def read_ressources(v):
         ressources[row[0].value] = Ressource(row[1].value,row[2].value,row[3].value,row[4].value,row[5].value,row[6].value)
     return ressources
 
-def read_unavalabilities():
-
+def read_ressources_unavalabilities(v):
+    ressources={}
     # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook("InstancesV1/InstanceBordeauxV1.xlsx")
+    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/InstanceBordeauxV"+str(v)+".xlsx")
 
     # Define variable to read the active sheet:
     worksheet = wookbook['Employees Unavailabilities']
@@ -28,11 +30,37 @@ def read_unavalabilities():
     for row in worksheet.iter_rows(2, max_row=15):
         if row[0].value == None:
             break
-        EmployeeName = row[0].value
-        latitude = row[1].value
-        longitude = row[2].value
-        start = row[3].value
-        end = row[4].value
+        ressources[row[0].value] = Ressource(row[1].value,row[2].value,row[3].value,row[4].value)
+        return ressources
 
-    print('ok')
-read_unavalabilities()
+def read_tasks():
+    ressources={}
+    # Define variable to load the wookbook
+    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/InstanceBordeauxV"+str(v)+".xlsx")
+
+    # Define variable to read the active sheet:
+    worksheet = wookbook['Tasks']
+
+    # Iterate the loop to read the cell values
+    for row in worksheet.iter_rows(2, max_row=15):
+        if row[0].value == None:
+            break
+        ressources[row[0].value] = Ressource(row[1].value,row[2].value,row[3].value,row[4].value,row[5].value,row[6].value)
+        return ressources
+
+def read_tasks_unavalabilities():
+    ressources={}
+    # Define variable to load the wookbook
+    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/InstanceBordeauxV"+str(v)+".xlsx")
+
+    # Define variable to read the active sheet:
+    worksheet = wookbook['Tasks Unavailabilities']
+
+    # Iterate the loop to read the cell values
+    for row in worksheet.iter_rows(2, max_row=15):
+        if row[0].value == None:
+            break
+        ressources[row[0].value] = Ressource(row[1].value,row[2].value)
+        return ressources
+
+
