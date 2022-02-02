b8237fc6df070bcd9df3f8e4f9f95c80f1bad490
Thomas Ghobril <thomas.ghobril@gmail.com>
2022-02-02 16:55:04 +0100

    retrieve data

--- read_input.py
+++ read_input.py
@@ -1,27 +1,20 @@
-# Import openyxl module
 import openpyxl
+from classes import Ressource,RessourceUnavailibility,Task,TaskUnavailibility
 
-def read_employee():
 
+def read_ressources(v):
+    ressources={}
     # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook("InstancesV1/InstanceBordeauxV1.xlsx")
-
+    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/InstanceBordeauxV"+str(v)+".xlsx")
     # Define variable to read the active sheet:
     worksheet = wookbook.active
 
     # Iterate the loop to read the cell values
-
-
     for row in worksheet.iter_rows(2, max_row=15):
         if row[0].value == None:
             break
-        EmployeeName = row[0].value
-        latitude = row[1].value
-        longitude = row[2].value
-        skill = row[3].value
-        level = row[4].value
-        workingStartTime = row[4].value
-        workingEndTime = row[6].value
+        ressources[row[0].value] = Ressource(row[1].value,row[2].value,row[3].value,row[4].value,row[5].value,row[6].value)
+    return ressources
 
 def read_unavalabilities():
 
