35d2b150d2f4a50d1265b610dd3047f5fbb7dc93
Thomas Ghobril <thomas.ghobril@gmail.com>
2022-02-02 16:40:45 +0100

    classes

---
+++ classes.py
@@ -0,0 +1,43 @@
+### Ressources
+class RessourceUnavailibilities:
+  def __init__(self, latitude,longitude,unavailabilityStart,unavailabilityEnd):
+    self.latitude=latitude
+    self.longitude=longitude
+    self.unavailabilityStart=unavailabilityStart
+    self.unavailabilityEnd=unavailabilityEnd
+
+class Ressource:
+  def __init__(self, latitude,longitude,skill,level,workingStart,workingEnd):
+    self.latitude=latitude
+    self.longitude=longitude
+    self.skill=skill
+    self.level=level
+    self.workingStart=workingStart
+    self.workingEnd=workingEnd
+
+  unavailibilities=[]
+
+  def addUnavailability(self, unavailability):
+    self.unavailibilities.append(unavailability)
+
+
+### Tâches
+class TaskUnavailibilities:
+  def __init__(self, unavailabilityStart,unavailabilityEnd):
+    self.unavailabilityStart=unavailabilityStart
+    self.unavailabilityEnd=unavailabilityEnd
+
+class Task:
+  def __init__(self, latitude,longitude,duration,skill,level,openingTime,closingTime):
+    self.latitude=latitude
+    self.longitude=longitude
+    self.duration=duration
+    self.skill=skill
+    self.level=level
+    self.openingTime=openingTime
+    self.closingTime=closingTime
+
+  unavailibilities=[]
+
+  def addUnavailability(self, unavailability):
+    self.unavailibilities.append(unavailability)
