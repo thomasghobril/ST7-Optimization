ac162ec29d40731e6351ce69ea4a097e2c05c081
nyss <ines.dormoy@student-cs.fr>
2022-02-02 16:48:34 +0100

    added read_input

---
+++ read_input.py
@@ -0,0 +1,46 @@
+# Import openyxl module
+import openpyxl
+
+def read_employee():
+
+    # Define variable to load the wookbook
+    wookbook = openpyxl.load_workbook("InstancesV1/InstanceBordeauxV1.xlsx")
+
+    # Define variable to read the active sheet:
+    worksheet = wookbook.active
+
+    # Iterate the loop to read the cell values
+
+
+    for row in worksheet.iter_rows(2, max_row=15):
+        if row[0].value == None:
+            break
+        EmployeeName = row[0].value
+        latitude = row[1].value
+        longitude = row[2].value
+        skill = row[3].value
+        level = row[4].value
+        workingStartTime = row[4].value
+        workingEndTime = row[6].value
+
+def read_unavalabilities():
+
+    # Define variable to load the wookbook
+    wookbook = openpyxl.load_workbook("InstancesV1/InstanceBordeauxV1.xlsx")
+
+    # Define variable to read the active sheet:
+    worksheet = wookbook.active
+
+    # Iterate the loop to read the cell values
+
+
+    for row in worksheet.iter_rows(2, max_row=15):
+        if row[0].value == None:
+            break
+        EmployeeName = row[0].value
+        latitude = row[1].value
+        longitude = row[2].value
+        skill = row[3].value
+        level = row[4].value
+        workingStartTime = row[4].value
+        workingEndTime = row[6].value
