2cd0eb46f6c64e28d1f4fda4b49eb0feddc3db27
Thomas Ghobril <thomas.ghobril@gmail.com>
2022-02-21 14:56:10 +0100

    flo

--- classes.py
+++ classes.py
@@ -1,15 +1,15 @@
 ### Ressources
 class RessourceUnavailability:
   def __init__(self, latitude,longitude,unavailabilityStart,unavailabilityEnd):
-    self.latitude=latitude
-    self.longitude=longitude
+    self.latitude=float(latitude)
+    self.longitude=float(longitude)
     self.unavailabilityStart=unavailabilityStart
     self.unavailabilityEnd=unavailabilityEnd
 
 class Ressource:
   def __init__(self, latitude,longitude,skill,level,workingStart,workingEnd):
-    self.latitude=latitude
-    self.longitude=longitude
+    self.latitude=float(latitude)
+    self.longitude=float(longitude)
     self.skill=skill
     self.level=level
     self.workingStart=workingStart
