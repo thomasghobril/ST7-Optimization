9b2aa3bb76bb52168e664f8cf41a53f463f4a763
Thomas Ghobril <thomas.ghobril@gmail.com>
2022-03-30 15:44:53 +0200

    ok

--- read_input2.py
+++ read_input2.py
@@ -1,29 +1,31 @@
 # Import openyxl module
 import openpyxl
-import datetime 
-from classes import Ressource,RessourceUnavailability,Task,TaskUnavailability
+import datetime
+from classes2 import Ressource, RessourceUnavailability, Task, TaskUnavailability
 
-INF=100
+INF = 100
+
 
 def time_process(string):
-    time=string.split(":")
-    if time[1][2]=='p':
-        time[1]=int(time[1][:2])
-        time[0]=int(time[0])
-        if time[0]==12:
-            time[0]=0
-        time[0]=time[0]+12
+    time = string.split(":")
+    if time[1][2] == 'p':
+        time[1] = int(time[1][:2])
+        time[0] = int(time[0])
+        if time[0] == 12:
+            time[0] = 0
+        time[0] = time[0]+12
     else:
-        time[1]=int(time[1][:2])
-        time[0]=int(time[0])
+        time[1] = int(time[1][:2])
+        time[0] = int(time[0])
     time = 60*time[0]+time[1]
     return time
 
 
-def read_ressources(v,ville):
-    ressources={}
+def read_ressources(v, ville):
+    ressources = {}
     # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx".format(ville))
+    wookbook = openpyxl.load_workbook(
+        "InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx".format(ville))
 
     # Define variable to read the active sheet:
     worksheet = wookbook['Employees']
@@ -32,15 +34,18 @@ def read_ressources(v,ville):
     for row in worksheet.iter_rows(2, max_row=INF):
         if row[0].value == None:
             break
-        time1=time_process(row[5].value)
-        time2=time_process(row[6].value)
-        ressources[row[0].value] = Ressource(row[1].value,row[2].value,row[3].value,row[4].value,time1,time2)
+        time1 = time_process(row[5].value)
+        time2 = time_process(row[6].value)
+        ressources[row[0].value] = Ressource(
+            row[1].value, row[2].value, row[3].value, row[4].value, time1, time2)
     return ressources
 
-def read_ressources_unavailabilities(v,ville,ressources):
-    ressources_unavailabilities={}
+
+def read_ressources_unavailabilities(v, ville, ressources):
+    ressources_unavailabilities = {}
     # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx")
+    wookbook = openpyxl.load_workbook(
+        "InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx")
 
     # Define variable to read the active sheet:
     worksheet = wookbook['Employees Unavailabilities']
@@ -49,16 +54,20 @@ def read_ressources_unavailabilities(v,ville,ressources):
     for row in worksheet.iter_rows(2, max_row=INF):
         if row[0].value == None:
             break
-        time1=time_process(row[3].value)
-        time2=time_process(row[4].value)
-        ressources_unavailabilities[row[0].value] = RessourceUnavailability(row[1].value,row[2].value,time1,time2)
-        ressources[row[0].value].addUnavailability(ressources_unavailabilities[row[0].value])
+        time1 = time_process(row[3].value)
+        time2 = time_process(row[4].value)
+        ressources_unavailabilities[row[0].value] = RessourceUnavailability(
+            row[1].value, row[2].value, time1, time2)
+        ressources[row[0].value].addUnavailability(
+            ressources_unavailabilities[row[0].value])
     return ressources_unavailabilities
 
-def read_tasks(v,ville):
-    tasks={}
+
+def read_tasks(v, ville):
+    tasks = {}
     # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx")
+    wookbook = openpyxl.load_workbook(
+        "InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx")
 
     # Define variable to read the active sheet:
     worksheet = wookbook['Tasks']
@@ -67,15 +76,18 @@ def read_tasks(v,ville):
     for row in worksheet.iter_rows(2, max_row=INF):
         if row[0].value == None:
             break
-        time1=time_process(row[6].value)
-        time2=time_process(row[7].value)
-        tasks[row[0].value] = Task(row[1].value,row[2].value,row[3].value,row[4].value,row[5].value,time1,time2)
+        time1 = time_process(row[6].value)
+        time2 = time_process(row[7].value)
+        tasks[row[0].value] = Task(
+            row[1].value, row[2].value, row[3].value, row[4].value, row[5].value, time1, time2)
     return tasks
 
-def read_tasks_unavailabilities(v,ville,tasks):
-    tasks_unavailabilities={}
+
+def read_tasks_unavailabilities(v, ville, tasks):
+    tasks_unavailabilities = {}
     # Define variable to load the wookbook
-    wookbook = openpyxl.load_workbook("InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx")
+    wookbook = openpyxl.load_workbook(
+        "InstancesV"+str(v)+"/Instance"+ville+"V"+str(v)+".xlsx")
 
     # Define variable to read the active sheet:
     worksheet = wookbook['Tasks Unavailabilities']
@@ -84,13 +96,16 @@ def read_tasks_unavailabilities(v,ville,tasks):
     for row in worksheet.iter_rows(2, max_row=INF):
         if row[0].value == None:
             break
-        time1=time_process(row[1].value)
-        time2=time_process(row[2].value)
-        tasks_unavailabilities[row[0].value] = TaskUnavailability(time1,time2)
-        tasks[row[0].value].addUnavailability(tasks_unavailabilities[row[0].value])
+        time1 = time_process(row[1].value)
+        time2 = time_process(row[2].value)
+        tasks_unavailabilities[row[0].value] = TaskUnavailability(time1, time2)
+        tasks[row[0].value].addUnavailability(
+            tasks_unavailabilities[row[0].value])
     return tasks_unavailabilities
 
-def read_input(v,ville):
-    r,t=read_ressources(v,ville),read_tasks(v,ville)
-    ru,tu=read_ressources_unavailabilities(v,ville,r),read_tasks_unavailabilities(v,ville,t)
-    return r,ru,t,tu
+
+def read_input(v, ville):
+    r, t = read_ressources(v, ville), read_tasks(v, ville)
+    ru, tu = read_ressources_unavailabilities(
+        v, ville, r), read_tasks_unavailabilities(v, ville, t)
+    return r, ru, t, tu
